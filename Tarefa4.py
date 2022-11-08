e9331d1d9642b35348f67c60a84ac888493fe1a4
GuilhermePeinador <guipeinador@hotmail.com>
2022-11-08 15:59:18 -0300

    Primeiros testes com GA.

--- Tarefa4.py
+++ Tarefa4.py
@@ -1,24 +1,27 @@
 import pygad
 import numpy as np
 
+def Calc_Coefs(k, p):
+    A0 = (5 * k) / 8
+    A1 = k * (p - 0.5)
+    A2 = 3 * k / 8
+
+    CL = 2 * np.pi * (A0 + A1 / 2)
+    CM = np.pi / 4 * (A2 - A1)**2
+
+    return CL, CM
+
+
 def fitness_func(solution, solution_idx):
     k = solution[0]
-    P = solution[1]
+    p = solution[1]
 
-    A0 = (5*k)/8
-    A1 = k*(P - 0.5)
-    A2 = 3*np.pi/8
-
-    CL = 2*np.pi*(A0+A1/2)
+    CL, CM = Calc_Coefs(k, p)
     CLref = 1.4
-    CM = np.pi/4 * (A2 - A1)**2
 
-    fitness = - ( np.abs(CL-CLref) + np.abs(CM)**2 )
-    return fitness
-
+    return - (abs(CL - CLref) + CM**2)
 
 last_fitness = 0
-
 
 def on_generation(ga_instance):
     global last_fitness
@@ -28,12 +31,12 @@ def on_generation(ga_instance):
     last_fitness = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]
 
 
-ga_instance = pygad.GA(num_generations=500,
+ga_instance = pygad.GA(num_generations=50,
                        num_parents_mating=10,
                        fitness_func=fitness_func,
                        sol_per_pop=10,
                        num_genes=2,
-                       gene_space={"low": 0, "high": 5},
+                       gene_space=[{"low": 0, "high": 0.3}, {"low": 0.05, "high": 2.}],
                        mutation_by_replacement=True,
                        on_generation=on_generation)
 
